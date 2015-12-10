32e6011562dd80b8009abc93f42f135063b744a4
Kostas Paliouras <paliou05@gmail.com>
2015-12-10 13:29:45 +0200

    changes to main

--- mainprogram.py
+++ mainprogram.py
@@ -18,13 +18,30 @@ def get_users(query,selector):
     all_users = filedownloader.find_users(query,selector)
     return all_users
 
-def update_age():
+def update_age(all_users):
     for user in all_users:
         user_id = user["_id"]
         query0 = {"_id": user_id}
         selector0 = {"$set" : {"age" : randint(80,90)}}
         filedownloader.update_value(query0,selector0,False,False)
-
+        
+def upload_file():
+    file = choose_file()
+    fileuploader.txt_read(file+'.txt')
+    
+def download_file():
+    filedownloader.print_db()
+    query = {}
+    selector = {"first":1,"last":1,"age":1,"dod":1,"gender":1,"hair_colour":1,"occupation":1,"nationality":1,"_id":1}
+    all_users = get_users(query,selector)
+    
+def update_file():
+    update_age(all_users)
+    print "\nUpdated\n"
+    
+def create_file(users):
+    file = choose_file()
+    filedownloader.create_file(users,file+'.txt')
 
 if __name__ == '__main__':
     
@@ -32,28 +49,27 @@ if __name__ == '__main__':
     
     if option == 1:
         fileuploader = FileUploader()
-        file = choose_file()
-        fileuploader.txt_read(file+'.txt')
+        upload_file()
         
     elif option == 2:
         filedownloader = FileDownloader()
-        filedownloader.print_db()
-        query = {}
-        selector = {"first":1,"last":1,"age":1,"dod":1,"gender":1,"hair_colour":1,"occupation":1,"nationality":1,"_id":1}
-        all_users = get_users(query,selector)
+        download_file()
         
         update = input("Want to update age?Y/N\n")
     
         if update == "Y":
-            update_age()
-            print "\nUpdated\n"
-            all_users = get_users(query,selector)
-            file = choose_file()
-            filedownloader.create_file(all_users,file+'.txt')
+            query = {}
+            selector = {"first":1,"last":1,"age":1,"dod":1,"gender":1,"hair_colour":1,"occupation":1,"nationality":1,"_id":0}
+            users = get_users(query,selector)
+            update_age(all_users)
+            update_file(users)
+            create_file(users)
             
         elif update == "N":
-            file = choose_file()
-            filedownloader.create_file(all_users,file+'.txt')
+            query = {}
+            selector = {"first":1,"last":1,"age":1,"dod":1,"gender":1,"hair_colour":1,"occupation":1,"nationality":1,"_id":0}
+            users = get_users(query,selector)
+            create_file(users)
             
         else:
             exit()
